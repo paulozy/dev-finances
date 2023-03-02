6dd37bd373e9a944b47ffe2304b0fd2d558c4a7c
Paulo Abreu <paulo.santana@luby.software>
2023-03-02 11:21:17 -0300

    chore: 🔧 config trpc hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { trpc } from '@/utils/trpc'
+import type { AppProps, AppType } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App: AppType = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default trpc.withTRPC(App)
---
+++ src/utils/trpc.ts
@@ -0,0 +1,25 @@
+import { AppRouter } from '@/server/routers/_app'
+import { httpBatchLink } from '@trpc/client'
+import { createTRPCNext } from '@trpc/next'
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return ''
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    return `https://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
+
+  return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
+export const trpc = createTRPCNext<AppRouter>({
+  config({ ctx }) {
+    return {
+      links: [
+        httpBatchLink({
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    }
+  },
+  ssr: false,
+})
